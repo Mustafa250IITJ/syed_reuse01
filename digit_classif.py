4089209a22eb7546c58de07f03cf28cfd404d63a
m22aie250@iitj.ac.in <root@Mustafa>
2023-08-31 20:04:26 +0530

    modified digit_classif

--- digit_classif.py
+++ digit_classif.py
@@ -12,13 +12,13 @@ hand-written digits, from 0-9.
 
 from util_fun import load_and_visualize_digits, preprocess_data, train_and_predict, visualize_predictions, evaluate_classifier
 
-def main():
-    digits = load_and_visualize_digits()
-    data = preprocess_data(digits)
-    X_test, predicted, y_test = train_and_predict(data, digits.target)
-    visualize_predictions(X_test, predicted)
-    evaluate_classifier(y_test, predicted)
+##def main():
+digits = load_and_visualize_digits()
+data = preprocess_data(digits)
+X_test, predicted, y_test = train_and_predict(data, digits.target)
+visualize_predictions(X_test, predicted)
+evaluate_classifier(y_test, predicted)
 
-if __name__ == "__main__":
-    main()
+##if __name__ == "__main__":
+##    main()
 
